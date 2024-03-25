3985961a1846100949d031a3e647bcb6fb49d7ed
nexer nul <redpenguin4luca26@gmail.com>
2024-03-25 21:22:45 +0000

    skeleton2

--- second attempt.py
+++ second attempt.py
@@ -4,24 +4,21 @@ print("evaluate your daily consumption of water")
 #measurement = input("enter preferred measurment: liters, plastic bottles, glasses")
 #bottle   liter   glasses 
 #16.9oz  33.814oz   8oz
-d = int(input("enter number of days you can recall your consumption:"))
-if d < 2:
-    print("error")
-
 
 def daily():
     daynum = 0
-    for i in range(d):
+    for i in range(0,7):
         daynum = daynum+1
         days = int(input("amount of water " + (str(daynum)) + "days ago:"))
         dailyconsumption.append(days)
         os.system('clear')
     print (dailyconsumption)
 
-#daily(measurement)
-#def daily(prefmeasurement):
+userconsumptionlist = daily()
 
-print(daily())
+def average():
+
+print(userconsumptionlist)
 #def calculations 
 
 #input("enter your daily consumption of water in liters for past 7 days")
