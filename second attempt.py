62b87723e8cac64c80d6c0e1644ac9dad8a6355a
nexer nul <redpenguin4luca26@gmail.com>
2024-04-25 05:00:16 +0000

    refined (no comments/extra variable)

--- second attempt.py
+++ second attempt.py
@@ -11,33 +11,34 @@ red = '\033[91m'
 yellow = '\033[93m'
 green = '\033[92m'
 bold = '\033[1m'
-reset = '\033[0m'
 msrs = {'liters':34, 'bottles':17, 'glasses':8}
 
-def record_data(determination, prompt, list):
-#data_list.clear()
+def record_data(prompt, determination, list):
     for i in range (1,8):
-        entry = int(input(prompt + " " + determination + f' {i} days ago:'))
-        list.append(entry)
-        os.system('clear')
+        while True:
+            try:
+                entry = int(input(prompt + " " + determination + f' {i} days ago:'))
+                list.append(entry)
+                os.system('clear')
+                break
+            except ValueError:
+                print("invald, this only accepts integers as input")
 
 def segment_ii():
-    print(blue + bold + "evaluate your daily consumption of water"+ reset)
+    print(blue + bold + "evaluate your daily consumption of water")
     inpmsr = input(bluue+ bold+"enter your preffered liquid measurment (liters, bottles, or glasses): ")
     os.system('clear')
 
-    record_data(inpmsr, "enter your", daily_consumption)
-    record_data("", "enter the number hours of exercise/physical activity you underwent",daily_xercise)
+    record_data("enter your", inpmsr, daily_consumption)
+    record_data("enter the number hours of exercise/physical activity you underwent", "", daily_xercise)
 
     inpweight = int(input("enter your estimated body weight: "))
     os.system('clear')
 
     for i in daily_consumption:
         calc_consumption.append(i * msrs[inpmsr])
-
     for i in daily_xercise:
         calc_xercise.append(i * (60//30 * 12))
-
     for i in calc_xercise:
         reccomended_oz.append(inpweight//2+i)
     
@@ -47,15 +48,16 @@ def segment_ii():
 
 def calc_avrg(list, inpmsr):
     avrg = sum(list)/len(list)
+    avrg_reccomended = (sum(reccomended_oz) / len(reccomended_oz))
     rounded_avrg = round(avrg)
     rounded_input = round(rounded_avrg/msrs[inpmsr])
     severity= ''
-    
-    if (sum(reccomended_oz) / len(reccomended_oz)) *0.4 >= avrg:
+
+    if avrg_reccomended *0.4 >= avrg:
         severity = red
-    elif (sum(reccomended_oz) / len(reccomended_oz)) *0.7 >= avrg:
+    elif avrg_reccomended *0.7 >= avrg:
         severity = yellow
-    else:
+    else:   
         severity = green
     if list == daily_xercise:
         return f'{rounded_avrg} hours,'
@@ -63,3 +65,4 @@ def calc_avrg(list, inpmsr):
         return f'{severity}{rounded_avrg} ounces{blue} (about {rounded_input} {inpmsr})'
 
 segment_ii()
+
